54af0640921dcb236eb3197c98c62dce7783c5fc
Julian Kwieciński <jkwiecinski@linxlogic.com>
2020-09-17 15:08:34 +0200

    gitignore

--- android-mvi-example/dal/build/tmp/kapt3/stubs/debug/com/example/dal/manager/DalManagerKt.java
+++
@@ -1,9 +0,0 @@
-package com.example.dal.manager;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 4, 0}, bv = {1, 0, 3}, k = 2, d1 = {"\u0000\n\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\"\u000e\u0010\u0000\u001a\u00020\u0001X\u0082T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0002\u001a\u00020\u0001X\u0082T\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0003"}, d2 = {"BASE_URL", "", "DATABASE_NAME", "dal_debug"})
-public final class DalManagerKt {
-    private static final java.lang.String BASE_URL = "https://www.omdbapi.com/";
-    private static final java.lang.String DATABASE_NAME = "movie-database";
-}
